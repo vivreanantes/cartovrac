ce4cd9da7f549816d7b9247c96743fe99d41cab6
Guillaume Bouju <40029016+GuillaumeBouju@users.noreply.github.com>
2018-12-30 22:54:57 +0100

    Update_Calendar

--- calendar.js
+++ calendar.js
@@ -23,45 +23,52 @@
 var today = new Date();
       var textDiv = "";
       if (Array.isArray(type_event)) {
-                 for (var i = 0; i < type_event.length; i++) {
+        var cptEventsFiltered = 0;
+                 for (var cptEventsNotFiltered = 0; cptEventsNotFiltered < type_event.length; cptEventsNotFiltered++) {
                      var show = true;
 
                      if (typeof getQueryParam("department") == "undefined") {
-                       if (type_event[i].department!="Loire-Atlantique") {
+                       if (type_event[cptEventsNotFiltered].department!="Loire-Atlantique") {
                          show = false;
                        }
                      }
                      if (show==true) {
-                       textDiv += "<a id='calendar_" + type_event[i].slug + "'>&nbsp</a>";
-                       textDiv += "<div class='wrap'><div class='truc'>";
-                       if (type_event[i].thumbnail == false) {
+                       cptEventsFiltered++;
+                       textDiv += "<div class='ligne'>";
+                       if(cptEventsFiltered%2 == 1){
+                         textDiv += "<a id='calendar_" + type_event[cptEventsNotFiltered].slug + "'>&nbsp</a>";
+                       } else {
+                         textDiv += "<a id='calendar_" + type_event[cptEventsNotFiltered].slug + "'></a>";
+                       }
+                       textDiv += "<div class='wrap'><div class='truc'><span class='image'>";
+                       if (type_event[cptEventsNotFiltered].thumbnail == false) {
                          textDiv += "&nbsp;";
                        }
                        else {
-                         textDiv += "<img src='"+type_event[i].thumbnail+"' width='100px' style='max-width:100px;height:auto' />";
+                         textDiv += "<img src='"+type_event[cptEventsNotFiltered].thumbnail+"' width='100%' style='max-width:100%;height:auto' />";
                        }
-                       textDiv += "</div><div class='truc'>";
-                       textDiv += "<b>"+type_event[i].title.fr+"</b>";
-                       textDiv += "<br/>"+type_event[i].description.fr;
-                       /*if (typeof type_event[i].keywords !== "undefined") {
-                         textDiv += "<br/><br/><i>"+type_event[i].keywords.fr+"</i>";
+                       textDiv += "</span></div><div class='truc'><span class='description'>";
+                       textDiv += "<b>"+type_event[cptEventsNotFiltered].title.fr+"</b>";
+                       textDiv += "<br/>"+type_event[cptEventsNotFiltered].description.fr;
+                       /*if (typeof type_event[cptEventsNotFiltered].keywords !== "undefined") {
+                         textDiv += "<br/><br/><i>"+type_event[cptEventsNotFiltered].keywords.fr+"</i>";
                        }*/
-                       textDiv += "</div><div class='truc'>";
+                       textDiv += "</span></div><div class='truc'><span class='timeDate'>";
                        var nbEvent = 0;
-                       for(var j = 0; j < type_event[i].timings.length; j++) {
-                         var eventStartDate = new Date(type_event[i].timings[j].start);
+                       for(var cptDateOfTheEvent = 0; cptDateOfTheEvent < type_event[cptEventsNotFiltered].timings.length; cptDateOfTheEvent++) {
+                         var eventStartDate = new Date(type_event[cptEventsNotFiltered].timings[[cptDateOfTheEvent]].start);
                          var timeDiff = eventStartDate.getTime() - today.getTime();
                          var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
                          if (diffDays>=0 && diffDays<=120 && nbEvent<6) {
                            textDiv += "- " + weekday[eventStartDate.getDay()];
-                           textDiv += " " + type_event[i].timings[j].start.substring(8,10) + " ";
-                           var mois = type_event[i].timings[j].start.substring(5,7).replace("01", "janv.")
+                           textDiv += " " + type_event[cptEventsNotFiltered].timings[[cptDateOfTheEvent]].start.substring(8,10) + " ";
+                           var mois = type_event[cptEventsNotFiltered].timings[cptDateOfTheEvent].start.substring(5,7).replace("01", "janv.")
                             .replace("02", "fév.").replace("03", "mars").replace("04", "avril").replace("05", "mai").replace("06", "juin").replace("07", "juil.")
                             .replace("08", "aôut").replace("09", "sept.").replace("10", "oct.").replace("11", "nov.").replace("12", "déc.");
                            textDiv += mois + " ";
-                           textDiv += type_event[i].timings[j].start.substring(0,4);
-                           textDiv += ", "+type_event[i].timings[j].start.substring(11,16);
-                           textDiv += " à " + type_event[i].timings[j].end.substring(11,16);
+                           textDiv += type_event[cptEventsNotFiltered].timings[cptDateOfTheEvent].start.substring(0,4);
+                           textDiv += ", "+type_event[cptEventsNotFiltered].timings[cptDateOfTheEvent].start.substring(11,16);
+                           textDiv += " à " + type_event[cptEventsNotFiltered].timings[cptDateOfTheEvent].end.substring(11,16);
                            textDiv += "<br/>";
                          }
                          if (nbEvent==6) {
@@ -69,18 +76,22 @@ var today = new Date();
                          }
                          nbEvent++;
                        }
-                       textDiv += "&nbsp;</div><div class='truc'>";
+                       textDiv += "&nbsp;</span></div><div class='truc'><span class='readMore'>";
                        textDiv += "<address>";
-                       textDiv += type_event[i].locationName;
+                       textDiv += type_event[cptEventsNotFiltered].locationName;
                        textDiv += " : ";
-                       textDiv += type_event[i].address;
+                       textDiv += type_event[cptEventsNotFiltered].address;
                        textDiv += "</address><br/>";
-                       if (type_event[i].conditions != null) {
-                         textDiv += "<b>Conditions</b> : " + type_event[i].conditions.fr;
+                       if (type_event[cptEventsNotFiltered].conditions != null) {
+                         textDiv += "<b>Conditions</b> : " + type_event[cptEventsNotFiltered].conditions.fr;
                        }
-                       textDiv += "<br/><br/><a href='" + type_event[i].canonicalUrl + "' target=_new><i>Plus d'infos</i></a>";
+                       textDiv += "<br/><br/><a href='" + type_event[cptEventsNotFiltered].canonicalUrl + "' target=_new><i>Plus d'infos</i></a>";
                        textDiv += "</div></div>";
-                       textDiv += "<div class='clear'></div>";
+                       textDiv += "</span></div>";
+
+                       if(cptEventsFiltered%2 == 0){
+                         textDiv += "<div class='clear'></div>";
+                       }
                      }
       }
     }
