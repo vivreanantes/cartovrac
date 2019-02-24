d37d50b5fe476a8517e7cd8ae52677e648cfa3ea
AntoineJaury <antoine@jaury.eu>
2019-02-24 21:36:42 +0100

    new: add script to copy data to dist folder

---
+++ copy_file_to_dist.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp cache_jtb_data.json ./dist/
+cp cache_bulk_data.json ./dist/
+cp itinerant.json ./dist/
+cp jtb_partners.json ./dist/
+cp assets/img/logo-carte.png ./dist/
---
+++ copy_files_to_dist.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp cache_jtb_data.json ./dist/
+cp cache_bulk_data.json ./dist/
+cp itinerant.json ./dist/
+cp jtb_partners.json ./dist/
