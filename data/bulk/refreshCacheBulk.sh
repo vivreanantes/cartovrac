87ff7493912ccacf1902d2da28313fde63d9bc15
AntoineJaury <antoine@jaury.eu>
2019-04-13 11:59:19 +0200

    chg: refactor build for data

--- data/bulk/refreshCacheBulk.sh
+++ data/bulk/refreshCacheBulk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -o logGetBulkContent.txt -O cache_bulk_data.json 'https://www.overpass-api.de/api/interpreter?data=[out:json][timeout:500][maxsize:20737418];(area[%22ISO3166-1%22=%22FR%22];)-%3E.fr;(node[%22bulk_purchase%22~%22yes|only%22](area.fr);way[%22bulk_purchase%22~%22yes|only%22](area.fr););out%20center;'
+wget -o ./dist/logGetBulkContent.txt -O ./dist/cache_bulk_data.json 'https://www.overpass-api.de/api/interpreter?data=[out:json][timeout:500][maxsize:20737418];(area[%22ISO3166-1%22=%22FR%22];)-%3E.fr;(node[%22bulk_purchase%22~%22yes|only%22](area.fr);way[%22bulk_purchase%22~%22yes|only%22](area.fr););out%20center;'
 
--- data/copy_files_to_dist.sh
+++ data/copy_files_to_dist.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cp ./data/partners/jtb/cache_jtb_data.json ./dist/
 cp ./data/partners/jtb/jtb_partners.json ./dist/
-cp ./data/bulk/cache_bulk_data.json ./dist/
+cp ./data/partners/cyclad/cyclad_partners.json ./dist/
 cp ./data/bulk/itinerant.json ./dist/
