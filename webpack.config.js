cd9fcb3873e612409c684b0a95266ae269cc6690
kaiyou <pierre@jaury.eu>
2020-02-03 17:41:06 +0100

    Add a gitlab ci script

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const webpack = require("webpack");
+const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  output: {
+    "path": path.resolve(__dirname, 'public'),
+  },
   module: {
     rules: [
       {
